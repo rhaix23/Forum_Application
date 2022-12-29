7eceb4cb6c1be133be3300001fc5a35ef69ef3c6
rhaix23 <patrick.lloyd.santos@gmail.com>
2022-12-30 02:41:23 +0800

    Pagination Component Updated

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -5,10 +5,18 @@ import { IQueryOptionsAction, QueryOptionTypes } from "../types/post.types";
 interface IProps {
   page: number;
   totalPages: number;
-  handleClick: (value: IQueryOptionsAction) => void;
+  handleClick: (value: any) => void;
+  type: string;
 }
 
-const PaginationComponent = ({ page, totalPages, handleClick }: IProps) => {
+const PaginationComponent = ({
+  page,
+  totalPages,
+  type,
+  handleClick,
+}: IProps) => {
+  if (totalPages === 1) return null;
+
   return (
     <Group position="right" mt={16}>
       <Pagination
@@ -16,7 +24,7 @@ const PaginationComponent = ({ page, totalPages, handleClick }: IProps) => {
         page={page}
         onChange={(page) =>
           handleClick({
-            type: QueryOptionTypes.ACTIVEPAGE,
+            type,
             payload: page,
           })
         }
